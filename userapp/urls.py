6f10536f7249ca3068ab40bf124569594e3dd228
Alice-Githui <gnyokabi.alice@gmail.com>
2021-11-16 09:34:30 +0300

    create registration form, registration and login views and urls

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,6 +3,8 @@ from .models import *
 from . import views
 
 urlpatterns=[
+    path("", views.loginUser, name="loginUser"), 
+    path("registerUser/", views.registerUser, name="register-User"),
     path("homepage", views.index, name="homepage"),
 
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+# import forms
+from .forms import *
 
 # Create your views here.
 def index(request):
     return render(request, "app/index.html", {})
+
+
+def registerUser(request):
+    return render(request, "registration/register.html", {})
+
+
+def loginUser(request):
+    return render(request, "registration/login.html", {})
 
